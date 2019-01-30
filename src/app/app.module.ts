8acedfe3f805ae54780f27c6ef495723d11f0907
Alejandro <david1804997@gmail.com>
2019-01-30 10:02:59 -0500

    Estructura inicial de la aplicación. Rutas, y componentes. TODO: importar angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { ArtistDetailComponent } from './components/artist-detail/artist-detail.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ArtistDetailComponent,
+    NavbarComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
